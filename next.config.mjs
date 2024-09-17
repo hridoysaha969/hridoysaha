0bd3f887fb9ebece24c3099a5d50c9aac6fd5dc1
Hridoy Saha <hridoysaha969@gmail.com>
2024-09-17 16:10:50 +0600

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = {
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-    };
-  },
-  exportTrailingSlash: true,
-};
 
 export default nextConfig;
