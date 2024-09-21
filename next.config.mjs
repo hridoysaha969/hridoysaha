b426914d87c9604a869dc0f9caa9a917e795ba43
Hridoy Saha <hridoysaha969@gmail.com>
2024-09-21 15:08:46 +0600

    version update 0.2.4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Any custom Next.js configurations can go here
+  reactStrictMode: true,
+};
 
 export default nextConfig;
