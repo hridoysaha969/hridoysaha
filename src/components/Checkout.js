3159f37616c504f1417ad2f22c5900c6bb0efd6a
Hridoy Saha <hridoysaha969@gmail.com>
2024-09-23 21:00:54 +0600

    version update 0.3.2

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,14 +4,29 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { MdShoppingCart } from "react-icons/md";
 function Checkout() {
+  const [paymentObj, setPaymentObj] = useState({
+    number: "",
+    trxId: "",
+    email: "",
+  });
   const [selectedPackage, setSelectedPackage] = useState();
   const [method, setMethod] = useState("");
+  const [loading, setLoading] = useState(false);
   const searchParams = useSearchParams();
   const packageId = searchParams.get("package");
   useEffect(() => {
+    setLoading(true);
     const services = servicesArray.filter((item) => item.package === packageId);
     setSelectedPackage(services[0]);
+    setLoading(false);
   }, [packageId]);
+  const handlePlaceOrder = () => {
+    if (method === "bkash") {
+      console.log("Order complete with bkash", paymentObj);
+    } else if (method === "payoneer") {
+      console.log("Order complete with Payoneer", paymentObj);
+    }
+  };
 
   return (
     <article className={`contact active`}>
@@ -30,99 +45,139 @@ function Checkout() {
           <h3 className="h3">Order Summery</h3>
         </div>
 
-        <ol className={styles.timeline__list}>
-          <li className={styles.timeline__item}>
-            <h4 className={`h4 ${styles.timeline__item_title}`}>
-              {selectedPackage?.title}
-            </h4>
+        {!loading && (
+          <ol className={styles.timeline__list}>
+            <li className={styles.timeline__item}>
+              <h4 className={`h4 ${styles.timeline__item_title}`}>
+                {selectedPackage?.title}
+              </h4>
 
-            <span>
-              {"$"}
-              {selectedPackage?.price}
-            </span>
+              <span>
+                {"$"}
+                {selectedPackage?.price}
+              </span>
 
-            <p className={styles.timeline__text}>
-              {selectedPackage?.description}
-            </p>
-          </li>
-          <li className={styles.timeline__item}>
-            <h4 className={`h4 ${styles.timeline__item_title}`}>
-              Payment Method
-            </h4>
+              <p className={styles.timeline__text}>
+                {selectedPackage?.description}
+              </p>
+            </li>
+            <li className={styles.timeline__item}>
+              <h4 className={`h4 ${styles.timeline__item_title}`}>
+                Payment Method
+              </h4>
 
-            <div className={styles.payment__wrapper}>
-              <div className={styles.method__options}>
-                <label className={styles.input__lable}>
-                  <input
-                    type="radio"
-                    name="payment"
-                    onChange={() => setMethod("bkash")}
-                  />
-                  <span>bKash</span>
-                </label>
-                <label className={styles.input__lable}>
-                  <input
-                    type="radio"
-                    name="payment"
-                    onChange={() => setMethod("payoneer")}
-                  />
-                  <span>Payoneer</span>
-                </label>
+              <div className={styles.payment__wrapper}>
+                <div className={styles.method__options}>
+                  <label className={styles.input__lable}>
+                    <input
+                      type="radio"
+                      name="payment"
+                      onChange={() => setMethod("bkash")}
+                    />
+                    <span>bKash</span>
+                  </label>
+                  <label className={styles.input__lable}>
+                    <input
+                      type="radio"
+                      name="payment"
+                      onChange={() => setMethod("payoneer")}
+                    />
+                    <span>Payoneer</span>
+                  </label>
+                </div>
+
+                {method === "bkash" ? (
+                  <ul className={styles.service__list}>
+                    <li className={styles.service__item}>
+                      <div className={styles.service__content_box}>
+                        <p className={styles.service__item_text}>
+                          Please complete your bKash payment at first, then fill
+                          up the form below.{" "}
+                          {`[NOTE]: 1.85% "SEND MONEY" cost will be added with net price.`}{" "}
+                          bKash Personal Number : {"+8801828304973"}
+                        </p>
+                        <div className={styles.transaction}>
+                          <span>bKash Number</span>
+                          <input
+                            type="number"
+                            name="number"
+                            value={paymentObj.number}
+                            className={styles.form__input}
+                            placeholder="017XXXXXXXX"
+                            onChange={(e) =>
+                              setPaymentObj({
+                                ...paymentObj,
+                                number: e.target.value,
+                              })
+                            }
+                            required
+                          />
+                        </div>
+                        <div>
+                          <span>Transaction ID</span>
+                          <input
+                            type="text"
+                            name="trxId"
+                            value={paymentObj.trxId}
+                            className={styles.form__input}
+                            placeholder="TrnxID"
+                            onChange={(e) =>
+                              setPaymentObj({
+                                ...paymentObj,
+                                trxId: e.target.value,
+                              })
+                            }
+                            required
+                          />
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                ) : method === "payoneer" ? (
+                  <ul className={styles.service__list}>
+                    <li className={styles.service__item}>
+                      <div className={styles.service__content_box}>
+                        <p className={styles.service__item_text}>
+                          Please provide us your email to get Payment request in
+                          your mail directly
+                        </p>
+                        <div className={styles.transaction}>
+                          <span>Your email</span>
+                          <input
+                            type="email"
+                            name="email"
+                            value={paymentObj.email}
+                            className={styles.form__input}
+                            placeholder="Email"
+                            required
+                            onChange={(e) =>
+                              setPaymentObj({
+                                ...paymentObj,
+                                email: e.target.value,
+                              })
+                            }
+                          />
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                ) : null}
               </div>
-
-              {method === "bkash" ? (
-                <ul className={styles.service__list}>
-                  <li className={styles.service__item}>
-                    <div className={styles.service__content_box}>
-                      <p className={styles.service__item_text}>
-                        Please complete your bKash payment at first, then fill
-                        up the form below.
-                      </p>
-                      <p className={styles.service__item_text}>
-                        bKash Personal Number : {"+8801828304973"}
-                      </p>
-                      <div className={styles.transaction}>
-                        <span>bKash Number</span>
-                        <input
-                          type="number"
-                          className={styles.form__input}
-                          placeholder="017XXXXXXXX"
-                        />
-                      </div>
-                      <div>
-                        <span>Transaction ID</span>
-                        <input
-                          type="text"
-                          className={styles.form__input}
-                          placeholder="TrnxID"
-                        />
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              ) : method === "payoneer" ? (
-                <ul className={styles.service__list}>
-                  <li className={styles.service__item}>
-                    <div className={styles.service__content_box}>
-                      <p className={styles.service__item_text}>
-                        Please provide us your email to get Payment request in
-                        your mail directly
-                      </p>
-                      <div className={styles.transaction}>
-                        <span>Your email</span>
-                        <input
-                          type="email"
-                          className={styles.form__input}
-                          placeholder="Email"
-                        />
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              ) : null}
-            </div>
-          </li>
-        </ol>
+            </li>
+            <li className={styles.timeline__item}>
+              <h4 className={`h4 ${styles.timeline__item_title}`}>
+                Complete your order
+              </h4>
+              <button
+                className={styles.btn__order}
+                disabled={!method ? true : false}
+                onClick={handlePlaceOrder}
+              >
+                <span>Place Order</span>
+              </button>
+            </li>
+          </ol>
+        )}
       </section>
     </article>
   );
