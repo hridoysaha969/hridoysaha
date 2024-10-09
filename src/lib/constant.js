c2296390379f6c1d4f1d361cb26ac9889bb368af
Hridoy Saha <hridoysaha969@gmail.com>
2024-10-09 23:47:03 +0600

    version update 0.5.3

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,4 +1,4 @@
-import p1 from "@/assets/projectImg/project-5.png";
+import p1 from "@/assets/projectImg/project-1.jpg";
 import p2 from "@/assets/projectImg/project-2.png";
 export const servicesArray = [
   {
@@ -95,13 +95,49 @@ export const servicesArray = [
 export const projectArray = [
   {
     _id: 1,
-    title: "Food Delivery",
+    title: "CryptoByte",
+    sub_title: "Real-Time Crypto Market Updates & Insights.",
     category: "web-design",
-    description:
-      "Developed a full-stack Food Delivery web application using Next.js and MongoDB. The app features a user-friendly interface for browsing and ordering meals, secure user authentication, dynamic product listings, and a real-time order management system. Integrated a responsive design and optimized the backend for efficient data handling and seamless performance.",
-    technologies: "Next.js, MongoDB, Mongoose, APIs, JWT",
+    overview:
+      "CryptoByte is a cutting-edge cryptocurrency tracking web application that provides users with real-time updates on market trands, including 24-hour highs, lows and market cap on various coins. CryptoByte allows visitors to monitor the fast-moving world of cryptocurrency with ease.",
+    technologies: "Vite + React, API Integration, Firebase",
+    challanges: [
+      {
+        title: "Real-Time Data Management",
+        description:
+          "Fetching and displaying constantly updating data without compromising performance was a key challenge. Implementing efficient API fetching strategies was critical.",
+      },
+      {
+        title: "Authentication Security",
+        description:
+          "Ensuring a smooth, secure login experience using Firebase while protecting user data was an important priority.",
+      },
+      {
+        title: "UI/UX Optimization",
+        description:
+          "Balancing large amounts of crypto data and providing a clean, user-friendly interface for visitors to quickly access essential information.",
+      },
+    ],
+    solutions: [
+      {
+        title: "Fast Load Times with Vite",
+        description:
+          "Leveraging Vite's lightning-fast build tool ensured that CryotoByte loads quickly even with real-time updates.",
+      },
+      {
+        title: "Efficient API Fetching",
+        description:
+          "By using API data caching and error handling, the application maintains up-to-date crypto information while reducing API calls to improve performance.",
+      },
+      {
+        title: "User Authentication with Firebase",
+        description:
+          "Implementing Firebase allowed for easy integration of secure login and user management features, offering users a personalized experience.",
+      },
+    ],
     image: p1,
-    liveLink: "https://hsfoodstore.netlify.app/",
+    liveLink: "https://bytecrypto.netlify.app/",
+    githubLink: "https://github.com/hridoysaha969/cryptobyte",
   },
   {
     _id: 2,
