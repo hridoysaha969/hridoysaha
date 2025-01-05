dbe815c73e3cf44af587d5bd4e784b050e9e848e
Hridoy Saha <hridoysaha969@gmail.com>
2025-01-05 11:58:06 +0600

    bug fiexes

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -2,10 +2,11 @@ import { useEffect, useRef } from "react";
 
 const ScrollToTop = ({ children, dependencies = [], className = "" }) => {
   const scrollContainerRef = useRef(null);
+  console.dir(scrollContainerRef.current);
 
   useEffect(() => {
     if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
   }, [dependencies]);
   return (
